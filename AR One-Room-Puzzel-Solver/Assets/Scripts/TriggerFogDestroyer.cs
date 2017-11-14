0ef12dbd9b31b7a242217b5b4de6da37fea025f3
Xeweros <renner.lukas@gmail.com>
2017-11-14 15:58:35 +0100

    Blackfog

---
+++ AR One-Room-Puzzel-Solver/Assets/Scripts/TriggerFogDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFogDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Fog"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
