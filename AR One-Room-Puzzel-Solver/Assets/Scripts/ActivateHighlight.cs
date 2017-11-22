ea62268c337081a53c1af2cd47cb743fdcbe6528
Xeweros <renner.lukas@gmail.com>
2017-11-22 14:57:10 +0100

    new features
    key rotate
    key 3D Sound
    zombie guards door
    Delay screen

---
+++ AR One-Room-Puzzel-Solver/Assets/Scripts/ActivateHighlight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateHighlight : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Key"))
+        {
+            other.transform.gameObject.GetComponent<cakeslice.Outline>().enabled = true;
+        }
+    }
+}
---
+++ AR One-Room-Puzzel-Solver/Assets/Scripts/KeysRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysRotate : MonoBehaviour
+{
+    public float m_fRotationSpeed = 10.0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * m_fRotationSpeed * Time.deltaTime);
+	}
+}
